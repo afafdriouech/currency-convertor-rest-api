c1de8e806d82b7655fbdd82a72d0862b7ec60c85
afaf driouech <afaf.driouech@gmail.com>
2021-12-22 14:08:21 +0200

    result class

--- src/main/java/com/api/currency/ResultBody.java
+++ src/main/java/com/api/currency/ResultBody.java
@@ -2,4 +2,31 @@ package com.api.currency;
 
 public class ResultBody {
 
+	private String success;
+	private Query query;
+	private Double result;
+	public String getSuccess() {
+		return success;
+	}
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+	public Query getQuery() {
+		return query;
+	}
+	public void setQuery(Query query) {
+		this.query = query;
+	}
+	public Double getResult() {
+		return result;
+	}
+	public void setResult(Double result) {
+		this.result = result;
+	}
+	@Override
+	public String toString() {
+		return "ResultBody [success=" + success + ", query=" + query + ", result=" + result + "]";
+	}
+	
+	
 }
